5fec9a4edbd6d7772dcfc8d67f134f06bb9a89d9
ahnzhx <modunaeggu@naver.com>
2020-08-01 07:49:52 +0900

    making multiplier

--- src/main/java/com/java/tdd/Dollar.java
+++ src/main/java/com/java/tdd/Dollar.java
@@ -4,10 +4,10 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
-        // 컴파일만 되게 할 것이라 아무일 안해도 된다.
+        this.amount = amount;
     }
 
     void times(int multiplier){
-        amount = 5 * 2;
+        amount *= multiplier;
     }
 }
