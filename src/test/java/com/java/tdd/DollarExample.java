696af05f013976fb1c1d98097db09e49b1c56b1d
ahnzhx <modunaeggu@naver.com>
2020-08-01 07:54:20 +0900

    changing issue of value of dollar

--- src/test/java/com/java/tdd/DollarExample.java
+++ src/test/java/com/java/tdd/DollarExample.java
@@ -4,12 +4,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class chapter1 {
+public class DollarExample {
     @Test
     public void testMultiplication(){
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
+        five.times(3);
+        assertEquals(15, five.amount);
     }
 
 }
