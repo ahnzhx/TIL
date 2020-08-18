b17982c52c463249a376503efddd771ef78c280a
ahnzhx <modunaeggu@naver.com>
2020-08-18 14:03:57 +0900

    effective java stream

--- src/main/java/com/java/effectiveJava/item45/MersennePrime.java
+++ src/main/java/com/java/effectiveJava/item45/MersennePrime.java
@@ -3,6 +3,7 @@ package com.java.effectiveJava.item45;
 import java.math.BigInteger;
 import java.util.stream.Stream;
 
+import static java.math.BigInteger.ONE;
 import static java.math.BigInteger.TWO;
 
 public class MersennePrime {
@@ -10,6 +11,14 @@ public class MersennePrime {
         return Stream.iterate(TWO, BigInteger::nextProbablePrime);
     }
     public static void main(String[] args) {
+        primes().map(p -> TWO.pow(p.intValueExact()).subtract(ONE))
+                .filter(mersenne -> mersenne.isProbablePrime(50))
+                .limit(20)
+                .forEach(System.out::println);
 
+        primes().map(p -> TWO.pow(p.intValueExact()).subtract(ONE))
+                .filter(mersenne -> mersenne.isProbablePrime(50))
+                .limit(20)
+                .forEach( mp -> System.out.println(mp.bitLength()+": "+ mp));
     }
 }
