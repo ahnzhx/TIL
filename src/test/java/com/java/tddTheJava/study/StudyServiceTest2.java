02143f94f375d35ce208625e7b0fe05176a46856
ahnzhx <modunaeggu@naver.com>
2021-01-05 07:45:30 +0900

    example of test container

--- src/test/java/com/java/tddTheJava/study/StudyServiceTest2.java
+++ src/test/java/com/java/tddTheJava/study/StudyServiceTest2.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -35,23 +40,13 @@ public class StudyServiceTest2 {
     @Autowired
     StudyRepository studyRepository;
 
-//    @Autowired
-//    Environment environment;
-
     @Value("${container.port}")
-    int port;
-
-    // 이렇게 만들면 테스트마다 컨테이너를 만든다. static 을 붙여줘야 함
-//    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer().withDatabaseName("studyTest");
+    String port;
 
     @Container
-    static GenericContainer postgreSQLContainer = new GenericContainer("postgres")
-            .withExposedPorts(5432)
-            .withEnv("POSTGRES_PASSWORD", "studytest")
-            .withEnv("POSTGRES_DB", "studyTest");
-
-//    @Container
-//    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer().withDatabaseName("studyTest");
+    static DockerComposeContainer composeContainer =
+            new DockerComposeContainer(new File("src/test/resources/docker-compose.yml"))
+            .withExposedService("study-db", 5432);
 
     @BeforeEach
     void beforeEach(){
@@ -64,14 +59,15 @@ public class StudyServiceTest2 {
 
     @Test
     void createNewStudy(){
-
+        System.out.println("======");
+        System.out.println(port);
     }
 
     static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>{
 
         @Override
         public void initialize(ConfigurableApplicationContext context) {
-            TestPropertyValues.of("container.port="+postgreSQLContainer.getMappedPort(5432))
+            TestPropertyValues.of("container.port="+ composeContainer.getServiceHost("study-db", 5432))
                                 .applyTo(context.getEnvironment());
 
         }
